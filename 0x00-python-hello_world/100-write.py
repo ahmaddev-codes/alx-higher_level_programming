f11014b82c1553fc2248203889dde9c9325f56a0
iAhm-codes <omegabliss23@gmail.com>
2023-09-04 14:53:05 +0100

    Code: edited script files

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19", end="\n")
 sys.exit(1)
-print("\n")
