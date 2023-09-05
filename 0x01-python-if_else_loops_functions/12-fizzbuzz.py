1a6175e55c3a9e936692027f1bb92144ccaa441c
iAhm-codes <omegabliss23@gmail.com>
2023-09-06 00:07:14 +0100

    Code: corrected code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,12 +4,12 @@
 # prints buzz for multiples of 5 and
 # and fizzbuzz for multiples of both 3 and 5
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 == 0):
-            print("Fizz ", end="")
-        elif (number % 5 == 0):
-            print("Buzz ", end="")
-        elif (number % 3 == 0 and number % 5 == 0):
-            print("FizzBuzz ", end="")
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz', end=' ')
+        elif i % 3 == 0:
+            print('Fizz', end=' ')
+        elif i % 5 == 0:
+            print('Buzz', end=' ')
         else:
-            print("{} ".format(number), end="")
+            print(i, end=' ')
