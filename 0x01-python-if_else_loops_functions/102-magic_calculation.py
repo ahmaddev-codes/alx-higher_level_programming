8e3698c19eea9b30ab8cc63200ef08cf0956975e
iAhm-codes <omegabliss23@gmail.com>
2023-09-06 02:32:14 +0100

    Code: edited code

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-import dis
-
-# magic_calculation - Calculate values and show it in the preprocessor
-# @a: first value
-# @b: second value
-# @c: third value
-
-
 def magic_calculation(a, b, c):
-    if a < b:
+    if (a < b):
         return c
-    elif c > b:
+
+    if (c > b):
         return a + b
-    else:
-        return a * b - c
 
-
-dis.dis(magic_calculation)
+    return (a * b) - c
