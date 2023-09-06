62d6283c5b00f22cd3285907dabbc3a348f99afb
iAhm-codes <omegabliss23@gmail.com>
2023-09-06 22:21:53 +0100

    Code: simplified code for Floyd's cycle algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,24 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
   * check_cycle - Checks if a singly linked list has a cycle in it
   * @list: The singly linked list to check
   *
-  * Return: 1 if has a cycle in it or 0 if not
+  * Return: 1 if it has a cycle in it or 0 if not
   */
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle = list, *hare = list;
-	int found = 0;
+    listint_t *turtle = list, *hare = list;
 
-	while ((turtle && hare) && hare->next)
-	{
-		turtle = turtle->next;
+    while (hare && hare->next)
+    {
+        turtle = turtle->next;
+        hare = hare->next->next;
 
-		if (hare->next || hare->next->next)
-			hare = hare->next->next;
-		else
-			break;
+        if (turtle == hare)
+            return 1; // Cycle detected
+    }
 
-		if (turtle == hare)
-		{
-			found = 1;
-			break;
-		}
-	}
-
-	return (found);
+    return 0; // No cycle found
 }
