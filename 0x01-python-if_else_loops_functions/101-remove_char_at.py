4a0ebf7c230160b58e4cfd51788a33cb2e1a1077
iAhm-codes <omegabliss23@gmail.com>
2023-09-06 02:25:45 +0100

    Code: added code to remove character at an index

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+remove_char_at - A function to remove character at a particular index
+
+@str: The string to read
+@n: The index to remove from
+"""
+def remove_char_at(str, n):
+    new_string = ""
+
+    for i in range(len(str)):
+        if i != n:
+            new_string += str[i]
+
+    return new_string
