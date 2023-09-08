6e8d711f066aa639ff719582e77c90afcf869a11
iAhm-codes <omegabliss23@gmail.com>
2023-09-08 21:49:14 +0100

    Code: modified code in 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -14,11 +14,8 @@ def replace_in_list(my_list, idx, element):
     """
     array_length = len(my_list)
 
-    if idx < 0:
+    if idx < 0 or idx >= array_length:
         return my_list
-    elif idx > array_length:
-        return my_list
+
     else:
-        for i in range(array_length):
-            if i == idx:
-                my_list[i] = element
+        my_list[idx] = element
