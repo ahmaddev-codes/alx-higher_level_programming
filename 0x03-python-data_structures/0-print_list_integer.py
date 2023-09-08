08c6defa770c667a423de362ec6c4ffba6a077aa
iAhm-codes <omegabliss23@gmail.com>
2023-09-08 19:49:15 +0100

    Code: added code to print integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that prints all integers of a list.
+print_list_integer - Prints all integers of a list.
+@my_list: List of integers.
+"""
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
