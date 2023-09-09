d52622528eada5cd2c350af955e8910640a25760
iAhm-codes <omegabliss23@gmail.com>
2023-09-09 11:48:05 +0100

    Code: modified code in 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,38 +1,5 @@
 #!/usr/bin/python3
 
-
-def check_tuple(tuple_a=(), tuple_b=()):
-    """checks if tuple_a and tuple_b meet the condition for addition
-
-    Args:
-    @tuple_a: The first tuple
-    @tuple_b: THe second tuple
-
-    Returns:
-    The new implemented tuple
-    """
-    # Check tuple_a
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = (0, 0)
-        elif len(tuple_a) == 1:
-            tuple_a = (tuple_a[0], 0)
-
-    elif len(tuple_a) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-
-    #check tuple_b
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = (0, 0)
-        elif len(tuple_b) == 1:
-            tuple_b = (tuple_b[0], 0)
-
-    elif len(tuple_a) > 2:
-        tuple_b = (tuple_b[0], tuple_b[1])
-
-    return (tuple_a, tuple_b)
-
 def add_tuple(tuple_a=(), tuple_b=()):
     """adds 2 tuples
 
@@ -41,9 +8,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
     @tuple_b: THe second tuple
 
     Returns:
-    Addition of the with two tuples
+    Result of the addition of two tuples
     """
-    new_tuple = check_tuple(tuple_a, tuple_b)
-    result = ((new_tuple[0][0] + new_tuple[1][0]), \
-            (new_tuple[0][1] + new_tuple[1][1]))
+    tuple_a = check_tuple(tuple_a)
+    tuple_b = check_tuple(tuple_b)
+
+    result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return result
+
+
+def check_tuple(_tuple=()):
+    if len(_tuple) < 2:
+        if len(_tuple) == 1:
+            _tuple = (_tuple[0], 0)
+        elif len(_tuple) == 0:
+            _tuple = (0, 0)
+    elif len(_tuple) > 2:
+        _tuple = (_tuple[0], _tuple[1])
+
+    return _tuple
