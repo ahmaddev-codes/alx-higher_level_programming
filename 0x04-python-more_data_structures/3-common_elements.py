ee3249dc93deb0f5a6cc2cf86c310c56999ccd02
iAhm-codes <omegabliss23@gmail.com>
2023-09-13 19:12:38 +0100

    Code: added code to find common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets
+
+    Args:
+    @set_1: First list to compare
+    @set_2: Second list to compare
+
+    Return:
+    The common elements in the two lists
+    """
+    return set_1 & set_2
