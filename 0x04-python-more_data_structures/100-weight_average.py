9f29d8fc2ab4a87669693fbe23d3eb56b3989338
iAhm-codes <omegabliss23@gmail.com>
2023-09-14 02:21:25 +0100

    Code: corrected code to accept empty lists

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list == () or  my_list == 0:
+    if my_list == 0:
         return 0
 
     return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -21,3 +21,19 @@ def roman_to_int(roman_string):
                 last = elem[1]
 
     return num
+
+# Function calls
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
