d1961ffd0c98b5d8d2fbfb9a936b2dcababb374d
iahm-codes <omegabliss23@.com>
2023-11-24 12:11:49 +0100

    Update: format code for pycodestyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 """Square Class
 This class doesn't do anything for the moment
-
 """
 
 
 class Square:
+    """Square
+    Class dooesn't do anything for now
+    """
     pass
