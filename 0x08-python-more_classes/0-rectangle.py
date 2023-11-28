a09e0ab196f2f67cfea7f9be8708a0914ed0631d
iahm-codes <omegabliss23@.com>
2023-11-28 06:36:28 +0100

    Code: added code for pycodestyle linting

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-    A module that defines a class Ractangle that does nothing
+A module that defines a class Ractangle that does nothing
 
 """
 
 
 class Rectangle:
     """
-        Creates an empty Rectangle class
+    Creates an empty Rectangle class
 
     """
 
