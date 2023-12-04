c47233a1e0926916134811659f1b06554419d0cb
iahm-codes <omegabliss23@.com>
2023-12-04 23:42:58 +0100

    Update: added documentation to module

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A module that contains a function that returns the list of
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """
     Returns the list of available atrributes and methods of an object
