2f253f3bdb9aef42b72a89550a610b3431648d57
iahm-codes <omegabliss23@.com>
2023-12-05 01:27:33 +0100

    Code: implemented a class with a public method area that throws an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains an class BaseGeomtry
+"""
+
+
+class BaseGeometry:
+    """An empty class that returns nothing"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
