9354ef976752ee2f474020316cdff24d2527de4e
iahm-codes <omegabliss23@.com>
2023-12-06 03:58:09 +0100

    Code: created a function that stringify an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""
+This module contains a function that returns the JSON
+representation of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object
+
+    Args:
+        my_obj (string): The object to stringify
+    """
+    return json.dumps(my_obj)
