ea0e9648a6f56165a3783edebc0e9ccc92d76e1b
iahm-codes <omegabliss23@.com>
2024-01-01 23:00:12 +0100

    Code: implemented a singly linked list with python

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -33,6 +33,7 @@ class Node:
 
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """defines a singly linked list"""
 
@@ -47,7 +48,8 @@ class SinglyLinkedList:
             self.__head = new_node
         else:
             current = self.__head
-            while current.next_node is not None and current.next_node.data < value:
+            while current.next_node is not None and \
+                    current.next_node.data < value:
                 current = current.next_node
 
             new_node.next_node = current.next_node
@@ -56,11 +58,13 @@ class SinglyLinkedList:
     def __str__(self):
         result = ""
         current = self.__head
-        while current is not None:
-            result += str(current.data)
 
-            if current.next_node is not None:
-                    result += "\n"
+        while current is not None:
+            if current.next_node is None:
+                result += str(current.data)
+            else:
+                result += str(current.data) + "\n"
 
             current = current.next_node
+
         return result
