5098264702fa35a4e82dbc6cbf1b3ff13d8906d0
iahm-codes <omegabliss23@.com>
2024-01-02 00:45:29 +0100

    Update: added pycache and test files

---
+++ 0x06-python-classes/testfiles/6-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+Square = __import__('6-square').Square
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
+
+my_square_4 = Square(3, (1, -3))
+my_square_4.my_print()
+
+print("--")
+
+my_square_5 = Square(3, "Position")
+my_square_5.my_print()
+
+print("--")
+
+my_square_6 = Square(3, (1,))
+my_square_6.my_print()
+
+print("--")
+
+my_square_7 = Square(3, (1, "3"))
+my_square_7.my_print()
