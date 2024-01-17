fd236718909c9af3b46599e5867a00bf9f2263a3
iahm-codes <omegabliss23@.com>
2024-01-17 04:04:28 +0100

    Update: query for advanced tasks

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,18 @@
+-- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
+USE `hbtn_0c_0`;
+
+-- Convert the database to utf8mb4
+ALTER DATABASE `hbtn_0c_0`
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table first_table to utf8mb4
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field in first_table to utf8mb4
+ALTER TABLE `first_table`
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
