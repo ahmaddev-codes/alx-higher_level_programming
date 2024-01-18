de70e5edfbe463b2e7ece8bdc4d12f7440e2f9ef
iahm-codes <omegabliss23@.com>
2024-01-18 02:48:22 +0100

    Code: added checks to no fail if database doesn't exist

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,10 @@
 -- deletes the database hbtn_0c_0 in MySQL server
+
+-- create databse
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- use databse
+USE hbtn_0c_0;
+
+-- drop databse
 DROP DATABASE IF EXISTS hbtn_0c_0;
