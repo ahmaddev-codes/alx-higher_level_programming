12fb0722d84e92e1f410a5dc18234f297b98d6a2
iahm-codes <omegabliss23@.com>
2024-02-12 19:17:48 +0100

    Code: A script that converts a string and float to an int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+    const inputNumber = parseInt(process.argv[2]);
+
+    if (!isNaN(inputNumber) && Number.isInteger(inputNumber)) {
+        console.log(`My number: ${Math.floor(inputNumber)}`);
+    } else {
+    console.log("Not a number");
+    }
+} else {
+    console.log("Not a number");
+}
