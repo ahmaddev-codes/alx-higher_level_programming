95372e3e7d2eac253e6488f564eee6ea8cfbe2f2
iahm-codes <omegabliss23@.com>
2024-02-12 21:06:42 +0100

    Code: A function that calculates the factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const value = parseInt(process.argv[2]);
+let sum = 1;
+
+function factorial(n) {
+    if (n != 1 && n != 0) {
+        sum *= n;
+        factorial(n - 1);
+    }
+    return sum;
+}
+
+if (value) {
+    console.log(factorial(value));
+} else if (isNaN(value)) {
+    console.log(1);
+}
