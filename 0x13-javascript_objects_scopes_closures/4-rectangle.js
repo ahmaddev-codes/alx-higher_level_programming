0d7b2e49ca2785aed6033c2a38348b147b9c2837
iahm-codes <omegabliss23@.com>
2024-02-14 08:11:42 +0100

    Code: a rectangle class extended to a square class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+class Rectangle {
+  shape = 'X'
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let shape = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        shape += this.shape;
+      }
+
+      if (i < this.height - 1) {
+        shape += '\n';
+      }
+    }
+    console.log(shape);
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
