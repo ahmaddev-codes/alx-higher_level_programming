22caa76d4157586f67d28052566d39727582423f
iahm-codes <omegabliss23@.com>
2024-02-14 23:06:56 +0100

    Code: a script that reads file from the CLI and write into another file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,15 +8,9 @@ const fileB = process.argv[3];
 const fileC = process.argv[4];
 
 function readFile (file) {
-  try {
-    // Using fs.readFileSync for synchronous reading
-    const content = fs.readFileSync(file, 'utf8');
-    console.log(`File '${file}' read successfully`);
-    return content;
-  } catch (error) {
-    console.error(`Error reading file '${file}':`, error.message);
-    process.exit(1); // Exit with an error code
-  }
+  // Using fs.readFileSync for synchronous reading
+  const content = fs.readFileSync(file, 'utf8');
+  return content;
 }
 
 function writeFile (fileA, fileB, fileC) {
@@ -24,14 +18,8 @@ function writeFile (fileA, fileB, fileC) {
   const fileBContent = readFile(fileB);
   const content = `${fileAContent}\n${fileBContent}`;
 
-  try {
-    // Using fs.writeFileSync for synchronous writing
-    fs.writeFileSync(fileC, content);
-    console.log(`Content written to '${fileC}' successfully`);
-  } catch (error) {
-    console.error(`Error writing to file '${fileC}':`, error.message);
-    process.exit(1); // Exit with an error code
-  }
+  // Using fs.writeFileSync for synchronous writing
+  fs.writeFileSync(fileC, content);
 }
 
 writeFile(fileA, fileB, fileC);
